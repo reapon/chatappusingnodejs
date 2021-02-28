16c172460b0b8774006689699782269d961705bf
Ashraful Islam <reapon.idb@gmail.com>
2021-02-28 17:18:24 +0600

    Node.js chat application.
    Real Time Chat Application using Node.js. Here You can chat and Share files..

---
+++ index.js
@@ -0,0 +1,48 @@
+﻿var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+
+var usernames = {};
+
+io.sockets.on('connection', function (socket) {
+
+    socket.on('sendchat', function (data) {
+        io.sockets.emit('updatechat', socket.username, data);
+    });
+
+    socket.on('adduser', function (username) {
+        socket.username = username;
+        usernames[username] = username;
+        socket.emit('updatechat', 'mChat', 'Welcome ' + username + ' !! You have successfully connected :)');
+        socket.broadcast.emit('updatechat', 'mChat'
+            , username + ' has joined the chat!!');
+        io.sockets.emit('updateusers', usernames);
+    });
+
+    socket.on('disconnect', function () {
+        delete usernames[socket.username];
+        io.sockets.emit('updateusers', usernames);
+        socket.broadcast.emit('updatechat', 'mChat'
+            , socket.username + ' has left the chat!!');
+    });
+    socket.on('typing', function () {
+        socket.broadcast.emit('typing', socket.username);
+    })
+
+    socket.on('fileUpload', (data, imgurl) => {
+        io.sockets.emit('fileUpload', socket.username, data, imgurl);
+    });
+});
+
+var port = 8080;
+server.listen(port);
+console.log('Server Running On : ' + port);
---
+++ public/Scripts/default.js
@@ -0,0 +1,112 @@
+﻿/// <reference path="jquery-3.3.1.min.js" />
+
+var socket;
+var fileName;
+$(document).ready(function () {
+    socket = io.connect('http://localhost:8080');
+    socket.on('connect', addUser);
+    socket.on('updatechat', processMessage);
+    socket.on('updateusers', updateUserList);
+    socket.on('typing', feedback);
+    socket.on('fileUpload', sendFile);
+
+    $('#send').click(sendMessage);
+    $('#message').keypress(processEnterPress);
+    $('#message').on('keydown', processKeyDown);
+    $('#uploadFile').on('change', uploadFile);
+});
+
+function uploadFile(e) {
+    let file = e.target.files[0];
+    let fileReader = new FileReader();
+    fileReader.onload = (event) => {
+        fileName = event.target.result
+    };
+    fileReader.readAsDataURL(file);
+};
+
+function addUser() {
+    socket.emit('adduser', prompt("Type Your Name!!"));
+}
+
+function processMessage(username, data) {
+    document.querySelector('#feedback').innerHTML = '';
+    document.querySelector('#output_msg').innerHTML += '<b>' + username + ': </b>' + data + '<br>';
+}
+
+
+function updateUserList(data) {
+    $('#users').empty();
+    $.each(data, function (key, value) {
+        $('#users').append('<li>' + key + '</li>');
+    });
+}
+
+function sendMessage() {
+    var message = $('#message').val();
+    if (message != "") {
+        $('#message').val('');
+        socket.emit('sendchat', message);
+        $('#message').focus();
+        $('#message').val('');
+
+
+    } else if (fileName != "") {
+        sendImage(message, fileName)
+        fileName = "";
+        $('#message').focus();
+        $('#message').val('');
+
+    } else {
+        $('#message').val('');
+        $('#message').focus();
+
+    }
+
+    
+}
+
+
+function sendFile(username, data, imgurl) {
+    document.querySelector('#feedback').innerHTML = '';
+    let label = document.createElement('label');
+    let img = document.createElement('img');
+    let text = document.createElement('p');
+
+    text.innerHTML = data;
+    label.innerHTML = username;
+    label.setAttribute('id', 'userHeading');
+    text.setAttribute('id', 'msg');
+    img.setAttribute('id', 'userImg');
+
+    img.src = imgurl;
+    let br = document.createElement('br');
+
+    document.querySelector('#output_msg').appendChild(label);
+    document.querySelector('#output_msg').appendChild(text);
+    document.querySelector('#output_msg').appendChild(img);
+    document.querySelector('#output_msg').appendChild(br);
+
+    document.querySelector('#uploadFile').value = "";
+
+    $('#feedback').html('');
+}
+
+
+
+function sendImage(message, fileName) {
+    socket.emit('fileUpload', message, fileName);
+}
+function processEnterPress(e) {
+    if (e.which == 13) {
+        e.preventDefault();
+        sendMessage();
+    }
+}
+function processKeyDown() {
+    socket.emit('typing', '');
+}
+
+function feedback(data) {
+    $('#feedback').html('<p>' + data + ' is typing a message...</p>');
+}
